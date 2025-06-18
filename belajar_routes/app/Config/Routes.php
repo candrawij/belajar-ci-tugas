71b739db47cb5b80de1244f4655da26bf8c48c0b
candrawij <candrawijayanto340@gmail.com>
2025-06-18 22:50:10 +0700

    first commit

---
+++ belajar_routes/app/Config/Routes.php
@@ -0,0 +1,31 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
+
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login');
+$routes->get('logout', 'AuthController::logout');
+
+$routes->group('produk', ['filter' => 'auth'], function ($routes) { 
+    $routes->get('', 'ProdukController::index');
+    $routes->post('', 'ProdukController::create');
+    $routes->post('edit/(:any)', 'ProdukController::edit/$1');
+    $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+});
+
+$routes->group('user', ['filter' => 'auth'], function ($routes) { 
+    $routes->get('', 'UserController::index');
+    $routes->post('', 'UserController::create');
+    $routes->post('edit/(:any)', 'UserController::edit/$1');
+    $routes->get('delete/(:any)', 'UserController::delete/$1');
+});
+
+$routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
+$routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
+$routes->get('user', 'UserController::index', ['filter' => 'auth']);
